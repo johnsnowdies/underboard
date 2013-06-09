ab6116bd36319e95bf8a2c9f64c48c1ea011c074
Сергей Чанышев <akinorri@gmail.com>
2013-06-10 01:35:45 +0400

    Registration

---
+++ application/views/register_view.php
@@ -0,0 +1,34 @@
+
+<div class="container">
+    <?= $data?>
+    <div class="row">
+        <div class="span3"></div>
+        <div class="span6">
+            <form action="<?= "http://" . $_SERVER['SERVER_NAME'] . "/auth"; ?>" class="form-horizontal"
+                  method="post">
+                <div class="control-group">
+                    <label class="control-label" for="inputEmail">Email</label>
+
+                    <div class="controls">
+                        <input type="text" id="inputEmail" name="email" placeholder="Email">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="inputPassword">Пароль</label>
+
+                    <div class="controls">
+                        <input type="password" id="inputPassword" name="password" placeholder="Пароль">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="controls">
+                        <label class="checkbox">
+                            <input type="checkbox"> Запомнить меня
+                        </label>
+                        <button type="submit" class="btn">Войти</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="span3"></div>
+    </div>
