9104364ced02423903f680e6ec5e758af4cad696
Akinorri <akinorri@gmial.com>
2014-07-20 23:38:42 +0400

    User migration

---
+++ application/views/registerdone_view.php
@@ -0,0 +1,14 @@
+<div class="container">
+	
+    <?= $data?>
+    <div class="row">
+		<div class="span3"></div>
+		<div class="span6">
+			<div class="hero-unit">
+				<h1>Успех</h1>
+				<p>Вы можете воспользоваться новыми доступами, что бы войти на сайт.</p>
+				<a href="<?= "http://" . $_SERVER['SERVER_NAME'] . "/auth/logout"; ?>">Перезайти</a>
+			</div>
+		</div>
+	</div>
+</div>
